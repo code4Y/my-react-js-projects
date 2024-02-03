56a3c864eb0ba20da612fba7a7aa26004c2a2315
Yash . <thecodeyash@gmail.com>
2024-02-03 15:51:13 +0530

    React Router Dom Added & Defined Routes

--- MySpace Route/src/App.jsx
+++ MySpace Route/src/App.jsx
@@ -1,12 +1,27 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 function App() {
   return (
-    <>
-      <h1>MySpace Route</h1>
-    </>
-  )
+    // Use Router to enable routing
+    <Router>
+      {/* // Use Link to create navigation links */}
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </div>
+      {/* // Use Routes to render only one route at a time */}
+      <Routes>
+        {/* // Use Route to define the paths and element for each page */}
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>  
+    </Router>
+  );
 }
 
-export default App
+export default App;
