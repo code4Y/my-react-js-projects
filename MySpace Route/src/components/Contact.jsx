da6960a732914db8941664dbf6052fcc49e9886e
Yash . <thecodeyash@gmail.com>
2024-02-05 23:58:56 +0530

    Content in Home, About & Contact section Added

--- MySpace Route/src/components/Contact.jsx
+++ MySpace Route/src/components/Contact.jsx
@@ -1,9 +1,25 @@
 function Contact() {
   
   return(
-    <>
-      <h1>Contact</h1>
-    </>
+    <div className="Contact">
+        <h1>Let's Connect📞</h1>
+        <span>
+          <h4>GitHub: </h4>
+          <a href="https://github.com/code4Y/">https://github.com/code4Y</a>
+        </span>
+        <span>
+          <h4>Twitter: </h4>
+          <a href="https://twitter.com/_code4Y/">@_code4Y</a>
+        </span>
+        <span>
+          <h4>LinkedIn: </h4>
+          <a href="https://www.linkedin.com/in/yash-shah-ys">Yash</a>
+        </span>
+        <span>
+          <h4>HashNode: </h4>
+          <a href="https://yashcode4y.hashnode.dev/">Yash</a>
+        </span>
+    </div>
   );
 }
 
--- MySpace Route/src/components/Home.jsx
+++ MySpace Route/src/components/Home.jsx
@@ -1,9 +1,12 @@
 function Home() {
 
   return(
-    <>
-      <h1>Home</h1>
-    </>
+    <div className="Home">
+      {/* Image Credit: "https://www.freepik.com/free-photo/modern-residential-district-with-green-roof-balcony-generated-by-ai_40970640.htm#query=home&position=0&from_view=search&track=sph&uuid=99e08d32-5805-4f0f-a4c1-1482dd35c7f1" 
+      Image by vecstock on Freepik*/}
+      <img src="https://img.freepik.com/free-photo/modern-residential-district-with-green-roof-balcony-generated-by-ai_188544-10276.jpg?w=1060&t=st=1707156333~exp=1707156933~hmac=c2e0b83c27845502444916aadfc6ec2f1efa4dace665ba524a769f5aebdfb359" alt="Home Img" />
+      
+    </div>
   );
 }
 
