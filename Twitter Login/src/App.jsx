5634d658a013f81fe5afd6104021c3b3cfd209c9
Yash . <thecodeyash@gmail.com>
2024-02-07 23:57:53 +0530

    Twitter, Google, Apple login buttons Added

--- Twitter Login/src/App.jsx
+++ Twitter Login/src/App.jsx
@@ -4,7 +4,16 @@ function App() {
   return (
     <div className='App'>
       <div className="logo-box">
-        
+        <img src="./twitter.png" alt="logo" className='logo' />
+        <h2>Sign In To Twitter</h2>
+        <button>
+          <img src="./google.png" alt="logo" />
+          Sign in with Google
+        </button>
+        <button>
+          <img src="./apple.png" alt="logo" />
+          Sign in with Apple
+        </button>
       </div>
     </div>
   )
