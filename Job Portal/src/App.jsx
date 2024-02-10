83644f235403fd6700c7631355b43b2d5716392f
Yash . <thecodeyash@gmail.com>
2024-02-10 23:57:03 +0530

    Navbar Content Added

--- Job Portal/src/App.jsx
+++ Job Portal/src/App.jsx
@@ -3,9 +3,37 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      <h1>Job Portal</h1>
-    </>
+    <div className='container'>
+      <nav>
+        <div className='logo-container'>
+          <img src="./logo.png" alt="" />
+        </div>
+
+        <div className='menu-container'>
+          <ul>
+            <li>
+              <a href="#" className='active'>Products</a>
+            </li>
+            <li>
+              <a href="#">Solution</a>
+            </li>
+            <li>
+              <a href="#">Price</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className='btn-container'>
+          <button className='btn'>
+            Log In
+          </button>
+        </div>
+      </nav>
+      
+    </div>
   )
 }
 
