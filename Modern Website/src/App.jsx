fe11808ca8da603de9744258af3adc86fa499d1f
Yash . <thecodeyash@gmail.com>
2024-02-22 22:06:55 +0530

    Website Content and Placeholders Added

--- Modern Website/src/App.jsx
+++ Modern Website/src/App.jsx
@@ -2,8 +2,43 @@ import './App.css'
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Modern Website</h1>
+    <div className="container">
+      <nav>
+        <img src="" className="logo" alt="imaged" />
+        <ul>
+          <li>
+            <a href="">Traval Guide</a>
+          </li>
+          <li>
+            <a href="">Famous Places</a>
+          </li>
+          <li>
+            <a href="">Contact Us</a>
+          </li>
+        </ul>
+        <button className="btn">
+          <img src="" alt="icon" /> Bookings
+        </button>
+      </nav>
+      <div className="content">
+        <h1>
+          Beautiful
+          <br />
+          Places to explore
+        </h1>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book.
+        </p>
+        <form>
+          <input type="text" placeholder="Country Name" />
+          <button type="submit" className="btn">
+            Search
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
