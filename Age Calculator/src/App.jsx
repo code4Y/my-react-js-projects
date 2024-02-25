3f02abdaf3de0800b02a35057d9e3a4985b74274
Yash . <thecodeyash@gmail.com>
2024-02-25 19:54:42 +0530

    AgeCalculator component and App.css Added

--- Age Calculator/src/App.jsx
+++ Age Calculator/src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
+import AgeCalculator from './components/AgeCalculator'
 
 function App() {
   return (
     <div className="App">
-      <h1>Age Calculator</h1>
+      <AgeCalculator />
     </div>
   )
 }
---
+++ Age Calculator/src/components/AgeCalculator.jsx
@@ -0,0 +1,12 @@
+import './AgeCalculator.css'
+ 
+const AgeCalculator = () => {
+
+  return (
+    <>
+      <h1>Age Calculator Component</h1>
+    </>
+  );
+};
+ 
+export default AgeCalculator;
