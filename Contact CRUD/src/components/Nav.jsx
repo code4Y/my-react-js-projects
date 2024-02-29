b447849a53782dda802fbf2a5dcf73d196204ba8
Yash . <thecodeyash@gmail.com>
2024-02-29 23:54:46 +0530

    Nav and db.json Added

---
+++ Contact CRUD/src/components/Nav.jsx
@@ -0,0 +1,33 @@
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import "./index.css";
+ 
+const Nav = () => {
+  return (
+    <>
+      <section>
+        <Navbar bg="primary" expand="lg">
+          <Container className="p-3">
+            <Navbar.Brand className="text-white" href="#home">
+              YashCode4y
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse className="justify-content-end">
+              <Nav className="me-auto justify-content-end">
+                <Nav.Link className="text-white " href="/">
+                  Home
+                </Nav.Link>
+                <Nav.Link className="text-white" href="/view">
+                  Employee
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </section>
+    </>
+  );
+};
+ 
+export default Nav;
