402b7887b6a36cb043db67423983af66d759deaf
Yash . <thecodeyash@gmail.com>
2024-03-02 23:59:21 +0530

    Styling Added

--- Contact CRUD/src/components/Nave.jsx
+++ Contact CRUD/src/components/Nave.jsx
@@ -1,19 +1,19 @@
+import React from 'react';
 import { Container, Navbar, Nav } from 'react-bootstrap'; 
-import "./index.css";
- 
+
 const Nave = () => {
   return (
     <>
       <section>
         <Navbar bg="primary" expand="lg">
-          <Container className="p-3">
+          <Container fluid className="p-3"> {/* Use fluid for full-width container */}
             <Navbar.Brand className="text-white" href="#home">
               YashCode4y
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse className="justify-content-end">
               <Nav className="me-auto justify-content-end">
-                <Nav.Link className="text-white " href="/">
+                <Nav.Link className="text-white" href="/">
                   Home
                 </Nav.Link>
                 <Nav.Link className="text-white" href="/view">
@@ -27,5 +27,5 @@ const Nave = () => {
     </>
   );
 };
- 
+
 export default Nave;
