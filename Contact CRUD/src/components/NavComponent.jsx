2b1bc3b07ee2bb2d12ffe1300f2ee82cb5b106a8
Yash . <thecodeyash@gmail.com>
2024-03-06 23:39:07 +0530

    Added import for bootsrtap.min.css now Bootstarp works

--- Contact CRUD/src/components/NavComponent.jsx
+++ Contact CRUD/src/components/NavComponent.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css'; // imp to include
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
