d64ad7fac9e8ee601921aa77a36c28f6aeb68248
Yash . <thecodeyash@gmail.com>
2024-03-10 22:31:39 +0530

    SCSS, MUI and base Form Added

--- Modern Login Form/src/App.jsx
+++ Modern Login Form/src/App.jsx
@@ -1,12 +1,16 @@
-import './App.css'
+import './App.scss';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 function App() {
-    return (
-    <>
-      <div className="App">
-        <h1>Minimal Modern Login Form</h1>
+  return (
+    <Box component="form" noValidate autoComplete='off'>
+      <div className="container_white">
+        <div className="container_padding">
+          
+        </div>
       </div>
-    </>
+    </Box>
   )
 }
 
