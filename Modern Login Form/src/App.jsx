114c9409fe7e932b2ccc7d196d7dc0b96fcf881a
Yash . <thecodeyash@gmail.com>
2024-03-11 23:54:00 +0530

    Label, TextField and Icons Added

--- Modern Login Form/src/App.jsx
+++ Modern Login Form/src/App.jsx
@@ -7,7 +7,26 @@ function App() {
     <Box component="form" noValidate autoComplete='off'>
       <div className="container_white">
         <div className="container_padding">
-          
+          <h3 className="container_title">Back to your digital life</h3>
+          <p className="container_para">Choose one of the option to go</p>
+          <div className="input_card">
+            <TextField className="input" id="outlined-basic" label="Email" variant="outlined" />
+            <TextField className="input" id="outlined-basic" label="Password" type="password" variant="outlined" />
+          </div>
+          <div className="line">
+            <p className="line_para">Or continue with</p>
+          </div>
+          <div className="social_card">
+            <div className="google">
+              <img src="./google.png" alt="" className="social_img" />
+            </div>
+            <div className="meta">
+              <img src="./meta.png" alt="" className="social_img" />
+            </div>
+            <div className="apple">
+              <img src="./apple.png" alt="" className="social_img" />
+            </div>
+          </div>
         </div>
       </div>
     </Box>
