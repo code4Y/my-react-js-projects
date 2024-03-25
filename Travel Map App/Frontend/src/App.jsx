51e9552a9af79c04310c858b11ab62d0edca55b1
Yash . <thecodeyash@gmail.com>
2024-03-25 23:55:07 +0530

    ReactMapGL Added

--- Travel Map App/Frontend/src/App.jsx
+++ Travel Map App/Frontend/src/App.jsx
@@ -1,7 +1,20 @@
+import { useState } from 'react';
+import ReactMapGL from 'react-map-gl';
+
 function App() {
+  const [viewport, setViewport] = useState({
+    width: 400,
+    height: 400,
+    latitute: 37.7577,
+    longitude: -122.4376,
+    zoom: 8
+  });
   return (
     <div className="App">
-      <h1>Travel Map App</h1>
+      <ReactMapGL
+      {...viewport}
+      onViewStateChange={nextViewport => setViewport(nextViewport)} 
+      />
     </div>
   )
 }
