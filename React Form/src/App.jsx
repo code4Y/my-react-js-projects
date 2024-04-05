86a2a99767fa1f5a0f724318552bfeeeb5fbdd14
Yash . <thecodeyash@gmail.com>
2024-04-05 23:56:49 +0530

    FormInput component Added

--- React Form/src/App.jsx
+++ React Form/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import FormInput from './components/FormInput';
 
 function App() {
   return (
     <div className="App">
       <form>
-        
+        <FormInput />
       </form>
     </div>
   )
