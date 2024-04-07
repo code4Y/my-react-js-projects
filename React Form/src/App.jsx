b97313f53c2617c1db8b8c1389b0ad25c7cd4198
Yash . <thecodeyash@gmail.com>
2024-04-07 23:57:35 +0530

    Title corrected and useState for username Added

--- React Form/src/App.jsx
+++ React Form/src/App.jsx
@@ -1,11 +1,15 @@
+import { useState } from 'react';
 import './App.css'
 import FormInput from './components/FormInput';
 
 function App() {
+  const [username, setUsername] = useState("");
+
+  console.log(username);
   return (
     <div className="App">
       <form>
-        <FormInput placeholder="Username" />
+        <FormInput placeholder="Username" setUsername={setUsername} />
         <FormInput placeholder="Email" />
         <FormInput placeholder="Full Name" />
         <FormInput placeholder="Something Else" />
