86c741483620f40d41a6a9e8d9744b67a698360f
Yash . <thecodeyash@gmail.com>
2024-04-11 23:43:40 +0530

    Handling multiple inputs using useState Added

--- React Form/src/App.jsx
+++ React Form/src/App.jsx
@@ -1,25 +1,71 @@
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import './App.css'
 import FormInput from './components/FormInput';
 
 function App() {
-  // const [username, setUsername] = useState("");
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    birthday: "",
+    password: "",
+    confirmPassword: "",
+  });
 
   console.log("re-rendered");
   
   const handleSubmit = (e) => {
     e.preventDefault();
-    const data = new FormData(e.target);
-    console.log(Object.fromEntries(data.entries()));
   }
 
+  const inputs = [
+    {
+      id: 1,
+      name: "username",
+      type: "text",
+      placeholder: "Username",
+      label: "Username"
+    },
+    {
+      id: 2,
+      name: "email",
+      type: "text",
+      placeholder: "Email",
+      label: "Email"
+    },
+    {
+      id: 3,
+      name: "birthday",
+      type: "text",
+      placeholder: "Birthday",
+      label: "Birthday"
+    },
+    {
+      id: 4,
+      name: "password",
+      type: "password",
+      placeholder: "Password",
+      label: "Password"
+    },
+    {
+      id: 5,
+      name: "confirmPassword",
+      type: "password",
+      placeholder: "ConfirmPassword",
+      label: "ConfirmPassword"
+    }
+  ];
+
+  const onChange = (e) => {
+    setValues({...values, [e.target.name]: e.target.value});
+  };
+
+  console.log(values);
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
-        <FormInput name="username" placeholder="Username" />
-        <FormInput name="email" placeholder="Email" />
-        <FormInput name="fullname" placeholder="Full Name" />
-        <FormInput name="something" placeholder="Something Else" />
+        {inputs.map((input) => (
+          <FormInput key={input.id} {...input} value={values[input.name]} onChange={onChange} />
+        ))}
         <button>Submit</button>
       </form>
     </div>
--- React Form/src/components/FormInput.jsx
+++ React Form/src/components/FormInput.jsx
@@ -1,13 +1,11 @@
 import "./formInput.css";
 
 const FormInput = (props) => {
+  const { label, onChange, id, ...inputProps } = props;
   return (
     <div className="formInput">
-      {/*<label>{props.placeholder}</label>*/}
-      <input 
-        name={props.name}
-        placeholder={props.placeholder} 
-      />
+      <label>{label}</label>
+      <input {...inputProps} onChange={onChange} />
     </div>
   );
 }
